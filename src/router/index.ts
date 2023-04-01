10294c1bd564f87bddfb3e9a6aa93fbf8aa93848
yu256 <122450013+yu256@users.noreply.github.com>
2023-04-01 16:08:49 +0900

    routerは末尾スラッシュで統一する

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/blog',
+      path: '/blog/',
       name: 'blog',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
